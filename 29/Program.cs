e051eec0bfc7c7dd73eb1a66f7348aab19084d8f
azadei <azadei@code.edu.az>
2023-07-17 15:19:14 +0400

    1

---
+++ 29/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        List<string> cities = new List<string>
+        {
+            "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI",
+            "AMSTERDAM", "ABU DHABI", "PARIS", "NEW YORK"
+        };
+
+       
+        var groupedCities = cities.Select((value, index) => new { Value = value, Index = index })
+                                  .GroupBy(item => item.Index / 3)
+                                  .Select(group => string.Join("; ", group.Select(item => item.Value)));
+
+        
+        Console.WriteLine("Here is the group of cities:");
+        foreach (var group in groupedCities)
+        {
+            Console.WriteLine(group);
+            Console.WriteLine("-- here is a group of cities --");
+        }
+
+    }
+}
